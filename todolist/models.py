3a2d80826a5c52860fdc8b35268b347995639461
Zwogli <Mathiaskohler@web.de>
2024-03-21 09:39:31 +0100

    Create new model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,13 @@
+import datetime
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateField(default=datetime.date.today)
+    checked = models.BooleanField(default=False)
